85b309964a8e9e8c21649b7c270642db7b04be81
brat2 <1599401@uab.cat>
2025-04-03 19:29:50 +0200

    Updated cart link

--- www/framing.js
+++ www/framing.js
@@ -4,9 +4,19 @@ document.addEventListener('DOMContentLoaded', async() => {
     const params = new URLSearchParams(window.location.search);
     const objectID = params.get("objectID");
 
+    updateCartLink()
+
     if (!objectID) {
         window.location.href = "search.html";
         return;
     }
 
 });
+
+function updateCartLink() {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const link = document.getElementById("cart-link");
+    if (cart.length > 0) {
+      link.innerText = `Cart (${cart.length})`;
+    }
+}
